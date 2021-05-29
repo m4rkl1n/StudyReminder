974bed050d182f68a684ccbc62a50e7635cbd7b8
marky <18591@students.mrgs.school.nz>
2021-05-29 16:31:05 +1200

    add new activity design

---
+++ app/src/main/java/com/example/studyreminder/NewTask.java
@@ -0,0 +1,7 @@
+package com.example.studyreminder;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class NewTask {
+    TextInputLayout
+}
--- app/src/main/java/com/example/studyreminder/myDatabase.java
+++ app/src/main/java/com/example/studyreminder/myDatabase.java
@@ -9,34 +9,39 @@ import androidx.annotation.Nullable;
 public class myDatabase extends SQLiteOpenHelper {
 
     private Context context;
-    public static final String DB_name = "BookLibreary.db";
-    public static final int DB_version = 1;
+    private static final String DB_name = "BookLibreary.db";
+    private static final int DB_version = 1;
 
-    public static final String Table_Name = "my_library";
-    public static final String taskID = "_id";
-    public static final String title = "task title";
-    public static final String description= "description";
-    public static final String date= "due_date";
+    private static final String Table_Name = "my_library";
+    private static final String taskID = "_id";
+    private static final String title = "task title";
+    private static final String description= "description";
+    private static final String date= "due_date";
 
 
 
-    @Override
-    public String getDatabaseName() {
-
-    }
+//    @Override
+//    public String getDatabaseName() {
+//
+//    }
 
     public myDatabase(@Nullable Context context) {
         super(context, DB_name, null, DB_version);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
-
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + Table_Name +
+                " (" + taskID + " INTEGER PRIMARY KEY AUTOIN CREMENT, " +
+                title + "TEXT " +
+                description + "TEXT " +
+                date + "TEXT );";
+        db.execSQL(query);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + Table_Name);
+        onCreate(db);
     }
 }
